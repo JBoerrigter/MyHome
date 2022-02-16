1ca17389f9ab463636505288e234a9f6cc67b0e7
Jascha Boerrigter <jascha.boerrigter@hotmail.com>
2022-02-16 21:57:24 +0100

    Added Settings and changed styling

---
+++ MyHome.Web/Pages/Settings.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyHome.Web.Pages
+{
+    public class SettingsModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+    }
+}
