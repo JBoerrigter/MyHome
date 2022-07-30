7342c9bd1da2e001d089c148f35dc9464cf0b46e
Jascha Boerrigter <jascha.boerrigter@hotmail.com>
2022-07-30 13:55:32 +0200

    Added Home and Familyservice

---
+++ MyHome.Shared/IHomeService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MyHome.Shared
+{
+	public interface IHomeService
+	{
+		HomeViewModel Get(int id);
+		int Create(int familyId, string street, string number, string postalCode, string city);
+		void Delete(int id);
+	}
+}
+
