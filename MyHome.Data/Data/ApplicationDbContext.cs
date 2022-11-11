5b7253c7a5fd7d87acd1ee12b8055f55eb01c73b
Jascha Boerrigter <jascha.boerrigter@hotmail.com>
2022-11-11 18:54:36 +0100

    Removed warnings

--- MyHome.Data/Data/ApplicationDbContext.cs
+++ MyHome.Data/Data/ApplicationDbContext.cs
@@ -5,17 +5,17 @@ namespace MyHome.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public DbSet<Family> Families { get; set; }
-        public DbSet<House> Houses { get; set; }
+        public DbSet<Family> Families { get; set; } = null!;
+        public DbSet<House> Houses { get; set; } = null!;
 
-        public DbSet<Income> Incomes { get; set; }
-        public DbSet<IncomeType> IncomeTypes { get; set; }
+        public DbSet<Income> Incomes { get; set; } = null!;
+        public DbSet<IncomeType> IncomeTypes { get; set; } = null!;
 
-        public DbSet<Expense> Expenses { get; set; }
-        public DbSet<ExpenseType> ExpenseTypes { get; set; }
+        public DbSet<Expense> Expenses { get; set; } = null!;
+        public DbSet<ExpenseType> ExpenseTypes { get; set; } = null!;
 
-        public DbSet<MeterReading> MetersReadings { get; set; }
-        public DbSet<MeterReadingType> MetersReadingTypes { get; set; }
+        public DbSet<MeterReading> MetersReadings { get; set; } = null!;
+        public DbSet<MeterReadingType> MetersReadingTypes { get; set; } = null!;
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
