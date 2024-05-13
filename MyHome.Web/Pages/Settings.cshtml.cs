343e082bdee638927164a81aa509202d5361e713
Jascha Boerrigter <jascha.boerrigter@hotmail.com>
2024-05-13 22:05:38 +0200

    Fixed darkmode

--- MyHome.Web/Pages/Settings.cshtml.cs
+++ MyHome.Web/Pages/Settings.cshtml.cs
@@ -5,9 +5,29 @@ namespace MyHome.Web.Pages
 {
     public class SettingsModel : PageModel
     {
+        public bool IsDarkMode { get; set; }
+
         public IActionResult OnGet()
         {
+            if (Request.Cookies.TryGetValue("darkmode", out string darkModeValue))
+            {
+                IsDarkMode = bool.TryParse(darkModeValue, out bool result) && result;
+            }
+            else
+            {
+                IsDarkMode = false;
+            }
             return Page();
+        }
+
+        public IActionResult OnPost(bool darkmode)
+        {
+            CookieOptions options = new CookieOptions
+            {
+                Expires = DateTime.Now.AddYears(1)
+            };
+            Response.Cookies.Append("darkmode", darkmode.ToString(), options);
+            return RedirectToPage();
         }
     }
 }
--- MyHome.Web/wwwroot/js/sidebar.js
+++ MyHome.Web/wwwroot/js/sidebar.js
@@ -1,9 +1,7 @@
 ﻿const body = document.querySelector("body"),
     sidebar = body.querySelector("nav"),
     toggle = body.querySelector(".toggle"),
-    searchBtn = body.querySelector(".search-box"),
-    modeSwitch = body.querySelector(".toggle-switch"),
-    modeText = body.querySelector(".mode-text");
+    searchBtn = body.querySelector(".search-box");
 
 toggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
@@ -11,14 +9,4 @@ toggle.addEventListener("click", () => {
 
 searchBtn.addEventListener("click", () => {
     sidebar.classList.remove("close");
-});
-
-modeSwitch.addEventListener("click", () => {
-    body.classList.toggle("dark");
-
-    if (body.classList.contains("dark")) {
-        modeText.innerText = "Light mode";
-    } else {
-        modeText.innerText = "Dark mode";
-    }
 });
